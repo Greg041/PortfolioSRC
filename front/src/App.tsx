eb906b91f9224dc56f1edb256bc02e319125d86c
Gregori <gregoriazuaje04@gmail.com>
2024-10-02 15:30:40 -0400

    added packages and conf to deploy in github pages

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,3 +1,6 @@
+// React imports
+import { BrowserRouter as Router } from 'react-router-dom'
+// Local imports
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
@@ -10,15 +13,17 @@ import Footer from './Components/Footer/Footer'
 function App() {
 
   return (
-    <div>
-      <Navbar />
-      <Hero />
-      <About />
-      <Services />
-      <MyWork />
-      <Contact />
-      <Footer />
-    </div>
+    <Router basename="/Portfolio">
+      <div>
+        <Navbar />
+        <Hero />
+        <About />
+        <Services />
+        <MyWork />
+        <Contact />
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
