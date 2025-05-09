8363e1b574c7d24e1f4e40a79932ecd1890d278e
Gregori <gregoriazuaje04@gmail.com>
2025-05-09 13:08:40 -0400

    agregadas descripciones de proyectos personales

--- src/consts/projects.const.ts
+++ src/consts/projects.const.ts
@@ -4,6 +4,8 @@ import instakash2 from "src/assets/works/instakash2.png";
 import instakash3 from "src/assets/works/instakash3.png";
 import alfabc from "src/assets/works/invest_landing.jpg";
 import devSearch from "src/assets/works/devsearch.jpg";
+import devSearch2 from "src/assets/works/devsearch2.png";
+import devSearch3 from "src/assets/works/devsearch3.png";
 import geeksZone from "src/assets/works/geeks_zone.jpg";
 import iqReader from "src/assets/works/iq_percentage_reader.png";
 import forexBot from "src/assets/works/bot_forex.jpg";
@@ -14,10 +16,12 @@ export const PROJECTS: ProjectInfo[] = [
     name: "Instakash",
     description:
       "Instakash es una plataforma intercambio de divisas localizada en Perú. " +
-      "Trabajé como desarrollador backend creando desde cero " +
-      "una plataforma de operaciones cambiarias para los empleados de Instakash " +
-      "dedicada al intercambio de divisas para empresas localizadas en el país." +
-      "Además de corregir bugs presentados en la plataforma principal usada por los usuarios naturales.",
+      "Trabajé como desarrollador backend desarrollando desde cero una API Rest a través de microservicios, " +
+      "una plataforma que incluye sistema de autenticación, creación " +
+      "de tasas de cambio para las divisas aceptadas por instakash y un sistema de gestión de operaciones " +
+      "cambiarias para que la empresa provea del servicio a otras empresas clientes localizadas en el país." +
+      "Además de corregir bugs presentados en la plataforma principal usada por los usuarios naturales y agregar " +
+      "documentación a su API.",
     projectImages: [instakash1, instakash2, instakash3],
     projectUrl: "https://instakash.net/",
     technologies: [
@@ -30,27 +34,37 @@ export const PROJECTS: ProjectInfo[] = [
     ],
   },
   {
-    pagePath: "alfabc",
-    name: "Invest Alfabc",
-    description: "",
+    pagePath: "Crowdinvest",
+    name: "Crowdinvest",
+    description:
+      "Plataforma de inversión crowdfunding enfocada en diversos tipos de activos financieros. " +
+      "Trabajé como desarrollador full stack manteniendo y mejorando funcionalidades existentes y arquitectura del proyecto, " +
+      "además de desarrollar nuevas funcionalidades para plataforma, " +
+      "desde la integración de una pasarela de pagos a través de paypal, crypto y transferencias bancarias, hasta " +
+      "la integración de Wagtail (cms desarrollado en django) agregando plantillas para las empresas clientes de " +
+      "CrowdInvest que quieran alojar su landing page en la plataforma,.",
     projectImages: [alfabc],
-    projectUrl: "",
+    projectUrl: "https://invest.alfabc.io/",
     technologies: ["Python", "Django", "HTML", "CSS", "Javascript", "HTMX"],
   },
   {
     pagePath: "devsearch",
     name: "Devsearch",
-    description: "",
-    projectImages: [devSearch],
+    description:
+      "Proyecto personal cuya finalidad es ayudar a los desarrolladores a encontrar " +
+      "oportunidades laborales. Contiene sistema de registro y login, edición del perfil para agregar habilidades " +
+      "técnicas, además de contar con una API la cual expone algunos endpoints para solicitar información de la Base de datos",
+    projectImages: [devSearch, devSearch2, devSearch3],
     projectUrl: "",
     technologies: ["Python", "Django", "HTML", "CSS", "Javascript"],
   },
   {
     pagePath: "geekszone",
     name: "La zona del geek",
-    description: "",
+    description:
+      "Proyecto personal el cual consiste en un blog de tecnología y otras noticias enfocadas en el mundo geek.",
     projectImages: [geeksZone],
-    projectUrl: "",
+    projectUrl: "https://geeks-blog.up.railway.app/posts/?page=1",
     technologies: ["Python", "Django", "HTML", "CSS", "Javascript"],
   },
   {
